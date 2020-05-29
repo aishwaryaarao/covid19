bca95841c63fd72894ff429b5bf9aba1b3b43c4f
AnandRRao <rranand@guruaditya.com>
2020-05-29 14:25:09 +0530

    Update Sequence

--- Sample.py
+++ Sample.py
@@ -2,5 +2,7 @@ import Bio
 from Bio.Seq import Seq
 from Bio.Alphabet import Alphabet
 
-my_seq = Seq("ATCGGACTTTAGGTG",Alphabet.letters)
-print(my_seq)
+my_seq = Seq("ATCGGACTTTAGGTG")
+print("Seq %s is %i bases long" % (my_seq,len(my_seq)))
+print("Reverse compliment is %s" % (my_seq.reverse_complement()))
+print("Protein translation is %s" % (my_seq.translate()))
